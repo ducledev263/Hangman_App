5204b6b97fa0c9cf0f3b6320050f941530325519
Darkie Lee <ducle0906.ulis@gmail.com>
2023-08-10 11:43:50 +0700

    add wordToGuess, styling and some apps for App.tsx

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <h1>asjdfhjas</h1>
-}
+import { useState } from "react"
+import words from "./wordList.json"
 
+function App() {
+  const [wordToGuess, setWordToGuess] = useState<string>(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  return <div 
+    style={{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center"
+  }}>
+    <div style={{font: "Arial", fontSize: "2rem", fontStyle: "italic"}}>You lose!!</div>
+    <HangmanDrawing />
+    <HangmanWord />
+    <Keyboard />
+  </div>
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+}
 export default App
