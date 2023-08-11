805d8f225878078ffa8053ed5ea39ad475d54bae
Darkie Lee <ducle0906.ulis@gmail.com>
2023-08-11 13:01:52 +0700

    add logic for HangmanWord

--- hangman/src/HangmanWord.tsx
+++ hangman/src/HangmanWord.tsx
@@ -1,6 +1,10 @@
-export function HangmanWord() {
-    const word = "Text"
-    const guessedLetters = ["e"]
+
+type HangmanWordProps = {
+    guessedLetters: string[],
+    wordToGuess: string
+}
+
+export function HangmanWord({guessedLetters, wordToGuess}: HangmanWordProps) {
     return <div 
     style={{
         display: "flex",
@@ -10,11 +14,12 @@ export function HangmanWord() {
         fontWeight: "bold",
         textTransform: "uppercase"
     }}>
-        {word.split("").map((letter, index) => (
+        {wordToGuess.split("").map((letter, index) => (
             <span style={{borderBottom: ".1em solid black"}} key={index}>
                 <span style={{visibility: guessedLetters.includes(letter)
-                ? "visible"
-                : "hidden"}}>{letter}</span>
+                    ? "visible"
+                    : "hidden"}}
+                >{letter}</span>
             </span>
         ))}
     </div>
