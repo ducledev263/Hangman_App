d842dc0d6e4c7155a0d7e46b633c17758fbabe3c
Darkie Lee <ducle0906.ulis@gmail.com>
2023-08-11 19:21:48 +0700

    add color and revealing

--- hangman/src/HangmanWord.tsx
+++ hangman/src/HangmanWord.tsx
@@ -1,10 +1,11 @@
 
 type HangmanWordProps = {
+    reveal?: boolean,
     guessedLetters: string[],
     wordToGuess: string
 }
 
-export function HangmanWord({guessedLetters, wordToGuess}: HangmanWordProps) {
+export function HangmanWord({reveal = false, guessedLetters, wordToGuess}: HangmanWordProps) {
     return <div 
     style={{
         display: "flex",
@@ -18,7 +19,9 @@ export function HangmanWord({guessedLetters, wordToGuess}: HangmanWordProps) {
             <span style={{borderBottom: ".1em solid black"}} key={index}>
                 <span style={{visibility: guessedLetters.includes(letter)
                     ? "visible"
-                    : "hidden"}}
+                    : "hidden"
+                color: !guessedLetters.includes(letter) && reveal ? "red" : "black"
+                }}
                 >{letter}</span>
             </span>
         ))}
